6832e4d1b4d8784dac1b4fabd6e889837f428445
Fabrem <fjbremermann@gmail.com>
2022-02-21 09:42:29 -0300

    Creacion del MovieSlice

--- src/features/store.js
+++ src/features/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./Movies/MovieSlice";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: moviesReducer,
 });
