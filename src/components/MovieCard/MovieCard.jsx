5503f95a5fb44631d2af56485ca3098fdde411d6
Fabrem <fjbremermann@gmail.com>
2022-02-21 11:03:23 -0300

    Styling y display del MovieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+//Estilos
+import "./MovieCard.css";
+
 const MovieCard = ({ data }) => {
-	return <div>Movie Card</div>;
+	return (
+		<div className="movie-card-item">
+			<section className="movie-card-inner">
+				<header className="movie-card-header">
+					<img src={data.Poster} alt={data.Title} />
+				</header>
+				<footer className="movie-card-footer">
+					<div className="movie-card-info">
+						<h4 className="movie-card-title">{data.Title}</h4>
+						<p>{data.Year}</p>
+					</div>
+				</footer>
+			</section>
+		</div>
+	);
 };
 
 export default MovieCard;
