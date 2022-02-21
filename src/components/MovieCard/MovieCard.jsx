564e5780cea0815b4021feeacc6b75e19e145277
Fabrem <fjbremermann@gmail.com>
2022-02-21 10:37:24 -0300

    Despliegue y estilos del MovieListing Component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MovieCard = () => {
+const MovieCard = ({ data }) => {
 	return <div>Movie Card</div>;
 };
 
--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -2,10 +2,34 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getAllMovies } from "../../features/Movies/MovieSlice";
 
+//Estilos
+import "./MovieListing.css";
+
+//Componentes
+import MovieCard from "../MovieCard/MovieCard";
+
 const MovieListing = () => {
 	const movies = useSelector(getAllMovies);
-	console.log(movies);
-	return <div>Movie Listing</div>;
+	let renderMovies = "";
+
+	renderMovies =
+		movies.Response === "True" ? (
+			movies.Search.map((movie, i) => (
+				<MovieCard key={`movie${i}`} data={movie} />
+			))
+		) : (
+			<div className="movies-error">
+				<h3>{movies.Error}</h3>
+			</div>
+		);
+	return (
+		<div className="movie-wrapper">
+			<div className="movie-list">
+				<h2>Movies</h2>
+				<div className="movie-container">{renderMovies}</div>
+			</div>
+		</div>
+	);
 };
 
 export default MovieListing;
