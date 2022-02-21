d6f526977ccb858e877eac6f1a5997cd43b78b4c
Fabrem <fjbremermann@gmail.com>
2022-02-21 09:48:31 -0300

    useDispatch para en home para despachar las peliculas de la api a la redux store

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { APIKey } from "../../services/api/MoviesApiKey";
 
 //Estilos
 import "./Home.css";
+
+//Slices
+import { addMovies } from "../../features/Movies/MovieSlice";
 
 //Componentes
 import MovieListing from "../MovieListing/MovieListing";
 import MoviesApi from "../../services/api/MoviesApi";
 
 const Home = () => {
-	useEffect(() => {
-		const movieText = "Harry";
+	const movieText = "Harry";
+	const dispatch = useDispatch();
 
+	useEffect(() => {
 		const fetchMovies = async () => {
 			const resp = await MoviesApi.get(
 				`?i=tt3896198&apiKey=${APIKey}&s=${movieText}&type=movie`
 			).catch((error) => console.error(error));
 
-			console.log(resp);
+			dispatch(addMovies(resp.data));
 		};
 
 		fetchMovies();
