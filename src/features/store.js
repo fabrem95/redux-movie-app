f6f9332bea0942ef7d3e914d00b79b2213d97de6
Fabrem <fjbremermann@gmail.com>
2022-02-23 12:47:39 -0300

    Codigo comentado para usar createApi, la cual no actualiza la store pero maneja el cacheo de peticiones de mejor manera

--- src/features/store.js
+++ src/features/store.js
@@ -1,6 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./Movies/MovieSlice";
+import { movieApi } from "../services/api/MoviesApi";
 
 export const store = configureStore({
-	reducer: { movies: moviesReducer },
+	reducer: {
+		movies: moviesReducer,
+		// [movieApi.reducerPath]: movieApi.reducer
+	},
+	// middleware: (getDefaultMiddleware) => {
+	// 	return getDefaultMiddleware().concat(movieApi.middleware);
+	// },
 });
--- src/services/api/MoviesApi.js
+++ src/services/api/MoviesApi.js
@@ -1,4 +1,38 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { APIKey } from "./MoviesApiKey";
 import axios from "axios";
+
+// export const movieApi = createApi({
+// 	reducerPath: "movieApi",
+// 	baseQuery: fetchBaseQuery({
+// 		baseUrl: `http://www.omdbapi.com/?&apiKey=${APIKey}&`,
+// 	}),
+// 	endpoints(builder) {
+// 		return {
+// 			fetchMovies: builder.query({
+// 				query(type = "movie", id = "tt3896198", search = "Harry") {
+// 					return `?i=${id}&s=${search}&type=${type}`;
+// 				},
+// 			}),
+// 			fetchShows: builder.query({
+// 				query(type = "series", id = "tt3896198", search = "Friends") {
+// 					return `?i=${id}&s=${search}&type=${type}`;
+// 				},
+// 			}),
+// 			fetchMovieOrShowDetails: builder.query({
+// 				query(id = "tt3896198", plot = "full") {
+// 					return `?i=${id}&Plot=${plot}`;
+// 				},
+// 			}),
+// 		};
+// 	},
+// });
+
+// export const {
+// 	useFetchMoviesQuery,
+// 	useFetchShowsQuery,
+// 	useFetchMovieOrShowDetailsQuery,
+// } = movieApi;
 
 export default axios.create({
 	baseURL: "http://www.omdbapi.com",
